a8b944bbaf245ac97bc40af6f3380efff5200d53
Tej Chajed <tchajed@mit.edu>
2019-08-29 17:56:02 -0400

    Simplify the test case

--- fscq-test.c
+++ fscq-test.c
@@ -9,13 +9,13 @@ int main(int argc, char *argv[])
 {
   chdir("/tmp/fscqmnt");
 
-  unsigned char buf[8192] = { 1, };
+  unsigned char buf[10] = { 1, };
   int fd_root = syscall(SYS_open, ".", O_DIRECTORY, 0);
   int fd_foo = syscall(SYS_open, "./foo", O_CREAT | O_RDWR, 0777);
-  syscall(SYS_fsync, fd_foo);
-  syscall(SYS_mkdir, "./A", 0777);
-  syscall(SYS_ftruncate, fd_foo, 5595);
-  syscall(SYS_pwrite64, fd_foo, buf, 4000, 1303);
+  // syscall(SYS_fsync, fd_foo);
+  // syscall(SYS_mkdir, "./A", 0777);
+  syscall(SYS_ftruncate, fd_foo, 4096);
+  syscall(SYS_pwrite64, fd_foo, buf, 2, 10);
   syscall(SYS_fsync, fd_root);
 
   system("killall -9 fscq"); // crash fscq
